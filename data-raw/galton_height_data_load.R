2087caad0d322b31038a3069bf2d24d0d40f10e4
David Kingman <davidkingman@Davids-Mac-mini.lan>
2022-07-19 22:24:45 +0100

    Changed how packages are attached and improved commenting.

--- data-raw/galton_height_data_load.R
+++ data-raw/galton_height_data_load.R
@@ -1,6 +1,6 @@
-# Install and attach packages
-if (!'pacman' %in% installed.packages()[, 'Package']) install.packages('pacman')
-pacman::p_load(char = c('tidyverse', 'here'), update = FALSE)
+# Attach packages
+library(tidyverse)
+library(here)
 
 # Import
 galton_height_data <- readr::read_tsv(here('data-raw', 'galton_height_data.txt'))
@@ -8,14 +8,12 @@ galton_height_data <- readr::read_tsv(here('data-raw', 'galton_height_data.txt')
 # Wrangle
 galton_height_data <- 
   galton_height_data %>% 
-  # Rename columns
   select(subject_height = Height,
          subject_father_height = Father,
          subject_mother_height = Mother,
          subject_sex = Gender,
          family_id = Family,
          number_of_children_in_family = Kids) %>% 
-  # Transform columns
   mutate(subject_sex = as_factor(subject_sex))
 
 # Export
